f17e18e993bcb1519b656fc7a94d3265bb6f2bb2
root <root@fantasy-nations.c.rhinelkc-165416.internal>
2017-04-25 16:18:57 +0800

    v0.0.5 move all to docker

--- rentmanager.sh
+++ rentmanager.sh
@@ -5,6 +5,8 @@ docker rm -f rentmanager
 # 启动并自动重启
 docker run \
  --name rentmanager \
+ --link myredis:myredis \
+ --link mymongo:mymongo \
  --expose 80 \
  --expose 443 \
  -v ~/gitcode/rent-manager:/webapp \
