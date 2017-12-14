3386804e2567bc8744c46c6dd07e3873b5b66a7a
rhinel <rhinelkc@hotmail.com>
2017-12-14 03:19:08 +0000

    调整生产脚本

--- autoFetch.sh
+++ autoFetch.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 cd ~/gitcode/rent-manager/
-git checkout test
-git push --all
+
+if [ "$TRAVIS_BRANCH" == "dev-0.1.0" ]; then
+  git checkout dev-0.1.0
+fi
+
+if [ "$TRAVIS_BRANCH" == "test" ]; then
+  git checkout test
+fi
+
+git pull --all
 yarn
+mkdir dist2
 tar -jxf dist.tar.bz2 -C dist2
 
 
