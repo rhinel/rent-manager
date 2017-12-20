fad71ed6e0047291847466736f4de1e6d112156b
rhinel <rhinelkc@hotmail.com>
2017-12-20 22:07:13 +0800

    增加注释的修改

--- server-node/config-log.js
+++ server-node/config-log.js
@@ -2,14 +2,15 @@ const path = require('path')
 
 module.exports = {
   appenders: {
-    // HTTP 请求日志
+    // HTTP日志，请求日志
     http: {
       type: 'dateFile',
       filename: `${path.resolve(__dirname, '../logs/httpLogs/logs')}Http.log`,
       maxLogSize: 102400,
       backups: 1024,
     },
-    // 系统日志，如：框架错误，catch程序错误，db连接错误等
+
+    // 系统日志，如：程序启动和db链接日志，框架错误，catch程序错误，db连接错误等
     sys: {
       type: 'dateFile',
       filename: `${path.resolve(__dirname, '../logs/sysLogs/logs')}Sys.log`,
@@ -17,14 +18,15 @@ module.exports = {
       backups: 1024,
     },
 
-    // 所有登录登出等的日志
+    // 登陆日志，所有登录登出等的日志
     login: {
       type: 'dateFile',
       filename: `${path.resolve(__dirname, '../logs/loginLogs/logs')}Login.log`,
       maxLogSize: 102400,
       backups: 1024,
     },
-    // 所有API请求产生的错误日志
+
+    // APIERROR日志，所有API请求产生的错误日志
     apiError: {
       type: 'dateFile',
       filename: `${path.resolve(__dirname, '../logs/apiErrorLogs/logs')}apiError.log`,
@@ -32,13 +34,14 @@ module.exports = {
       backups: 1024,
     },
 
-    // 所有的错误日志
+    // 错误日志，所有的错误日志
     errorsFile: {
       type: 'dateFile',
       filename: `${path.resolve(__dirname, '../logs/errorsFileLogs/logs')}ErrorsFile.log`,
       maxLogSize: 102400,
       backups: 1024,
     },
+
     errors: {
       type: 'logLevelFilter',
       appender: 'errorsFile',
