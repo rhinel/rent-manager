3696478926702c6595c5e42e8e97c68924b79978
rhinel <rhinelkc@hotmail.com>
2018-01-06 10:40:13 +0800

    抽取水电张贴组件

--- src/pcside/components/rent/rent-month.vue
+++ src/pcside/components/rent/rent-month.vue
@@ -671,139 +671,23 @@
       <el-tab-pane
         label="6坊65栋水电张贴"
         name="waterandeleCal6">
-        <div class="rent-count-title">
-          <div class="cal-title">
-            <div>6坊65栋{{ getCalMonth(monthDet.month) }}水电</div>
-            <div>抄表日期：{{ getTnewTime() }}</div>
-          </div>
-        </div>
-        <div>
-          <table class="cal-body">
-            <tr>
-              <th>房间号</th>
-              <th>本次水表</th>
-              <th>上次水表</th>
-              <th>实用数</th>
-              <th>小计</th>
-              <th />
-              <th>本次电表</th>
-              <th>上次电表</th>
-              <th>实用数</th>
-              <th>小计</th>
-            </tr>
-            <tr
-              v-for="item in monthDetData"
-              v-if="item.rents[0] && item.fang == '6坊65栋'"
-              :key="item._id">
-              <th>{{ item.hao }}</th>
-              <th>{{ item.rents[0].calWater ? item.rents[0].calWater.tnew.water : '--' }}吨</th>
-              <th>{{ item.rents[0].calWater ? item.rents[0].calWater.old.water : '--' }}吨</th>
-              <th>
-                {{
-                  item.rents[0].calWater ?
-                    (item.rents[0].calWater.tnew.water - item.rents[0].calWater.old.water) : '--'
-                }}吨
-              </th>
-              <th>
-                {{
-                  item.rents[0].calWater ? getCal(item.rents[0].calWater, 'water') : '--'
-                }}元
-              </th>
-              <th />
-              <th>
-                {{
-                  item.rents[0].calElectric ? item.rents[0].calElectric.tnew.electric : '--'
-                }}度
-              </th>
-              <th>
-                {{
-                  item.rents[0].calElectric ? item.rents[0].calElectric.old.electric : '--'
-                }}度
-              </th>
-              <th>
-                {{
-                  item.rents[0].calElectric ?
-                    item.rents[0].calElectric.tnew.electric -
-                    item.rents[0].calElectric.old.electric : '--'
-                }}度
-              </th>
-              <th>
-                {{
-                  item.rents[0].calElectric ? getCal(item.rents[0].calElectric, 'ele') : '--'
-                }}元
-              </th>
-            </tr>
-          </table>
-        </div>
+
+        <print-cal-table
+          :month-det="monthDet"
+          :month-det-data="monthDetData"
+          type-string="6坊65栋" />
+
       </el-tab-pane>
 
       <el-tab-pane
         label="8坊68栋水电张贴"
         name="waterandeleCal8">
-        <div class="rent-count-title">
-          <div class="cal-title">
-            <div>8坊68栋{{ getCalMonth(monthDet.month) }}水电</div>
-            <div>抄表日期：{{ getTnewTime() }}</div>
-          </div>
-        </div>
-        <div>
-          <table class="cal-body">
-            <tr>
-              <th>房间号</th>
-              <th>本次水表</th>
-              <th>上次水表</th>
-              <th>实用数</th>
-              <th>小计</th>
-              <th />
-              <th>本次电表</th>
-              <th>上次电表</th>
-              <th>实用数</th>
-              <th>小计</th>
-            </tr>
-            <tr
-              v-for="item in monthDetData"
-              v-if="item.rents[0] && item.fang == '8坊68栋'"
-              :key="item._id">
-              <th>{{ item.hao }}</th>
-              <th>{{ item.rents[0].calWater ? item.rents[0].calWater.tnew.water : '--' }}吨</th>
-              <th>{{ item.rents[0].calWater ? item.rents[0].calWater.old.water : '--' }}吨</th>
-              <th>
-                {{
-                  item.rents[0].calWater ?
-                    (item.rents[0].calWater.tnew.water - item.rents[0].calWater.old.water) : '--'
-                }}吨
-              </th>
-              <th>
-                {{
-                  item.rents[0].calWater ? getCal(item.rents[0].calWater, 'water') : '--'
-                }}元
-              </th>
-              <th />
-              <th>
-                {{
-                  item.rents[0].calElectric ? item.rents[0].calElectric.tnew.electric : '--'
-                }}度
-              </th>
-              <th>
-                {{
-                  item.rents[0].calElectric ? item.rents[0].calElectric.old.electric : '--'
-                }}度
-              </th>
-              <th>
-                {{
-                  item.rents[0].calElectric ?
-                    item.rents[0].calElectric.tnew.electric -
-                    item.rents[0].calElectric.old.electric : '--'
-                }}度
-              </th>
-              <th>
-                {{
-                  item.rents[0].calElectric ? getCal(item.rents[0].calElectric, 'ele') : '--'
-                }}元
-              </th>
-            </tr>
-          </table>
-        </div>
+
+        <print-cal-table
+          :month-det="monthDet"
+          :month-det-data="monthDetData"
+          type-string="8坊68栋" />
+
       </el-tab-pane>
     </el-tabs>
   </div>
@@ -820,6 +704,8 @@ import TableRentRemarkItem from 'pcside/common/table-rent-remark-item'
 import CollapseLandordItem from 'pcside/common/collapse-landord-item'
 import CollapseRentCountItem from 'pcside/common/collapse-rent-count-item'
 
+import PrintCalTable from 'pcside/common/print-cal-table'
+
 export default {
   name: 'RentMonth',
   components: {
@@ -830,6 +716,8 @@ export default {
 
     CollapseLandordItem,
     CollapseRentCountItem,
+
+    PrintCalTable,
   },
   mixins: [mixinDef],
   data() {
@@ -1300,6 +1188,7 @@ export default {
 
       this.gettingLandordRentTemp = false
     },
+    // 检查是否空计算合集
     checkObjectTemp(val) {
       if (val.sixList && val.sixList.length) return true
       if (val.eightList && val.eightList.length) return true
@@ -1342,75 +1231,11 @@ export default {
       if (typeof val !== 'object') return false
       return !!Object.keys(val).length
     },
-    // 获取月份
-    getCalMonth(monthStr) {
-      if (!monthStr) return ''
-      const date = new Date(monthStr)
-      date.setMonth(date.getMonth() - 1)
-      let month = date.getMonth() + 1
-      month = month > 9 ? month : `0${month}`
-      return `${date.getFullYear()}-${month}`
-    },
-    // 获取抄表日期，计租数据中第一条可用数据的时间
-    getTnewTime() {
-      const takeTime = this.monthDetData.find(item => item.rents[0] && item.rents[0].calWater)
-      if (!takeTime) return ''
-      return this.GetDateFormat(takeTime.rents[0].calWater.tnew.addTime)
-    },
-    // 计算张贴的价格
-    getCal(rent, type) {
-      // 用于张贴展示，不做真实计费，所有计费按月度周期来计算
-      const {
-        calType,
-        minPrice,
-        singlePrice,
-        stepPrice,
-      } =
-        type === 'water' ? (
-          this.monthDet.defaultCalWaterPrice ||
-          this.defaultCalWaterPrice
-        ) : (
-          this.monthDet.defaultCalElePrice ||
-          this.defaultCalElePrice
-        )
-
-      let gap = type === 'water' ?
-        (rent.tnew.water - rent.old.water) :
-        (rent.tnew.electric - rent.old.electric)
-      gap = Math.max(0, gap, minPrice)
-
-      let result = 0
-      if (calType === 'single') {
-        result = gap * singlePrice
-      } else if (calType === 'step') {
-        result = stepPrice.reduce((cal, price, index) => {
-          if (!price.price) return cal
-          const prevPrices = stepPrice[index - 1] || {}
-          if (
-            (gap > (prevPrices.step || 0) && gap <= price.step) ||
-            (index === (stepPrice.length - 1) && gap >= price.step)
-          ) {
-            return gap * price.price
-          }
-          return cal
-        }, result)
-      }
-      return Math.floor(Math.round(result * 100) / 100)
-    },
   },
 }
 </script>
 
 <style lang="scss">
-@media print {
-  /* 特殊打印样式 */
-  .rent-month {
-    .el-tabs__header {
-      display: none !important;
-    }
-  }
-}
-
 .rent-month {
   /* 弹窗表单样式 */
   .add-month-det-dialog,
@@ -1440,47 +1265,6 @@ export default {
   .change-type-dialog {
     .el-input {
       max-width: 300px;
-    }
-  }
-
-  /* 打印标题样式 */
-  .cal-title {
-    text-align: center;
-
-    div:first-child {
-      letter-spacing: 0.5em;
-      padding-bottom: 40px;
-      font-size: 40px;
-    }
-
-    div:last-child {
-      font-size: 20px;
-      padding-bottom: 40px;
-    }
-  }
-
-  /* 打印报表样式 */
-  .cal-body {
-    width: 100%;
-    font-size: 20px;
-
-    tr {
-      display: flex;
-      border-top: 1px solid #000;
-
-      &:last-child {
-        border-bottom: 1px solid #000;
-      }
-
-      th {
-        flex: 1;
-        border-left: 1px solid #000;
-        padding: 15px 0;
-
-        &:last-child {
-          border-right: 1px solid #000;
-        }
-      }
     }
   }
 }
