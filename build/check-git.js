62912c804653dd572c0754ca32e0da14c4d485df
rhinel <rhinelkc@hotmail.com>
2018-01-21 15:06:09 +0800

    test build

--- build/check-git.js
+++ build/check-git.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
 
 const gitHEAD = fs.readFileSync('.git/HEAD', 'utf-8').trim()
+console.log(gitHEAD)
 const ref = gitHEAD.split(': ')[1]
+console.log(ref)
 const gitVersion = fs.readFileSync(`.git/${ref}`, 'utf-8').trim()
+console.log(gitVersion)
 const gitCommitVersion = `${ref}: ${gitVersion}`
 
 module.exports = gitCommitVersion
