51c0ac9b07aa69ee46650152f311e3dd3fc75948
rhinel <rhinelkc@hotmail.com>
2019-03-17 21:36:22 +0800

    docs: git hooks修改为中文提示

--- scripts/verify-commit-msg.js
+++ scripts/verify-commit-msg.js
@@ -11,12 +11,12 @@ const commitRE = /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|wor
 if (!commitRE.test(msg)) {
   console.log()
   console.error(
-    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red('invalid commit message format.')}\n\n${
-    chalk.red('  Proper commit message format is required for automated changelog generation. Examples:\n\n')
-    }    ${chalk.green('feat(compiler): add \'comments\' option')}\n`
-    + `    ${chalk.green('fix(v-model): handle events on blur (close #28)')}\n\n${
-    chalk.red('  See .github/COMMIT_CONVENTION.md for more details.\n')
-    }${chalk.red(`  You can also use ${chalk.cyan('npm run commit')} to interactively generate a commit message.\n`)}`
+    `  ${chalk.bgRed.white(' 错误 ')} ${chalk.red('无效的提交消息格式。')}\n\n${
+    chalk.red('  自动更改日志生成需要正确的提交消息格式。例如：\n\n')
+    }    ${chalk.green('feat(编译器): 新增 \'评论\' 选项')}\n`
+    + `    ${chalk.green('fix(v-model): 处理模糊事件 (close #28)')}\n\n${
+    chalk.red('  查看文档 https://github.com/vuejs/vue/blob/dev/.github/COMMIT_CONVENTION.md 获取更多信息。\n\n')
+    }${chalk.red(`  或者你可以使用命令 ${chalk.cyan('npm run commit')} 以交互方式生成提交消息。\n`)}`
   )
   process.exit(1)
 }
